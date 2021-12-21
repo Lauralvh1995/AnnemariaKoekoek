6d1b17ea630df1ab34f0b5a80614d0676ad9490a
Laura van Helden <337797@student.fontys.nl>
2021-12-21 15:13:06 +0100

    fixed the table class to account for answer

--- Assets/Scripts/GameManagement/Table.cs
+++ Assets/Scripts/GameManagement/Table.cs
@@ -22,7 +22,7 @@ public class Table : MonoBehaviour
         equations.Clear();
         for(int i = 1; i < 11; i++)
         {
-            equations.Add(new Equation(i, Operator.Multiply, basis));
+            equations.Add(new Equation(i, Equation.Operator.Multiply, basis, i * basis));
         }
     }
 
